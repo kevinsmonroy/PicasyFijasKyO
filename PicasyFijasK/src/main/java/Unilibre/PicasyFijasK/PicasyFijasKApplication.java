c330b5143dc70c36a9af05bd81692cd019a9ff17
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2026-05-14 09:44:33 -0500

    Initial commit

---
+++ PicasyFijasK/src/main/java/Unilibre/PicasyFijasK/PicasyFijasKApplication.java
@@ -0,0 +1,13 @@
+package Unilibre.PicasyFijasK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicasyFijasKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicasyFijasKApplication.class, args);
+	}
+
+}
---
+++ PicasyFijasK/src/test/java/Unilibre/PicasyFijasK/PicasyFijasKApplicationTests.java
@@ -0,0 +1,13 @@
+package Unilibre.PicasyFijasK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicasyFijasKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
