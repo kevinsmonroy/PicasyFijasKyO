beb0ef4b875afa70deac13a0a5509e1758318f02
kevinsmonroy <kevins-monroyo@unilibre.edu.co>
2026-05-18 17:47:09 -0500

    arreglando el apliccation properties

--- PicasyFijasK/src/main/java/Unilibre/PicasyFijasK/PicasyFijasKApplication.java
+++ PicasyFijasK/src/main/java/Unilibre/PicasyFijasK/PicasyFijasKApplication.java
@@ -3,11 +3,10 @@ package Unilibre.PicasyFijasK;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "Unilibre.PicasyFijasK")
 public class PicasyFijasKApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PicasyFijasKApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PicasyFijasKApplication.class, args);
+    }
 }
